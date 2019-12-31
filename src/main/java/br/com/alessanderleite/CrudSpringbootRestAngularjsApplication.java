377d930c44db83481df00e48adb8894d6325b8ab
Alessander <alessander.logistica@gmail.com>
2019-12-31 15:59:24 -0300

    initial commit

---
+++ src/main/java/br/com/alessanderleite/CrudSpringbootRestAngularjsApplication.java
@@ -0,0 +1,13 @@
+package br.com.alessanderleite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringbootRestAngularjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringbootRestAngularjsApplication.class, args);
+	}
+
+}

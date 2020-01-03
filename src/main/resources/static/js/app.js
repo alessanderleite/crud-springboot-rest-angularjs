c4732f55f9761e4b980a84006f277b9ef1da32a2
Alessander <alessander.logistica@gmail.com>
2020-01-03 18:47:05 -0300

    create the function to call all employees

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,5 +1,32 @@
 var app = angular.module('EmployeeManagement', []);
 
-app.controller('EmployeeController', function() {
+//controller Part
+app.controller('EmployeeController', function($scope, $http) {
 	
+    $scope.employees = [];
+    $scope.employeeForm = {
+        empId: 1,
+        empNo: '',
+        empName: ''
+    };
+ 
+    // Now load the data from server
+    _refreshEmployeeData();
+
+    // Private Method  
+	// HTTP GET- get all employees collection
+	// Call: http://localhost:8080/employees
+	function _refreshEmployeeData() {
+	    $http({
+	        method: 'GET',
+	        url: '/employees'
+	    }).then(
+	        function(res) { // success
+	            $scope.employees = res.data;
+	        },
+	        function(res) { // error
+	            console.log("Error: " + res.status + " : " + res.data);
+	        }
+	    );
+	}
 });
